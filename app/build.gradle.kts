668e54db7980d76e5e635cbaa1ee8aa29d9fdd44
suchana-das014 <suchanadas541@gmail.com>
2025-08-04 01:52:33 +0600

    testing added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,4 +77,5 @@ dependencies {
     androidTestImplementation(project(":app"))
     androidTestImplementation(project(":app"))
     androidTestImplementation(project(":app"))
+    androidTestImplementation(project(":app"))
 }
