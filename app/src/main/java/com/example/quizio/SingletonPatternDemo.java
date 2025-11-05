b1ab4bc0c29c9c89fa04a16c4ee2129719341927
tam-a-nna <fahmidachowdhury207@gmail.com>
2025-11-06 01:06:29 +0600

    SingletonPattern Added

---
+++ app/src/main/java/com/example/quizio/SingletonPatternDemo.java
@@ -0,0 +1,28 @@
+package com.example.quizio;
+
+class QuizManager {
+    private static QuizManager instance;
+
+    private QuizManager() {}
+
+    public static QuizManager getInstance() {
+        if (instance == null) {
+            instance = new QuizManager();
+        }
+        return instance;
+    }
+
+    public void startQuiz() {
+        System.out.println("Quiz has started!");
+    }
+}
+
+public class SingletonPatternDemo {
+    public static void main(String[] args) {
+        QuizManager manager1 = QuizManager.getInstance();
+        manager1.startQuiz();
+
+        QuizManager manager2 = QuizManager.getInstance();
+        System.out.println(manager1 == manager2); // Output: true
+    }
+}
