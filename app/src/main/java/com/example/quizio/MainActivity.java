bc74d7d4592943cd2026eb111e67a9ba6280d471
tam-a-nna <fahmidachowdhury207@gmail.com>
2025-12-11 12:18:53 +0600

    main activity updated

--- app/src/main/java/com/example/quizio/MainActivity.java
+++ app/src/main/java/com/example/quizio/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);  // এইটা থাকবে
+        setContentView(R.layout.activity_main);
 
         btnPlay = findViewById(R.id.btnPlay);
         btnSettings = findViewById(R.id.btnSettings);
